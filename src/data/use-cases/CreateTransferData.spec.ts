6c6008d9a04c45cbb790ef6829e4198a19a2dc91
lucsimao <lucasimao64@gmail.com>
2022-01-03 19:05:53 -0300

    test: ensure throw when httpClient throws

---
+++ src/data/use-cases/CreateTransferData.spec.ts
@@ -0,0 +1,34 @@
+import { CreateTransferData } from './CreateTransferData';
+import { CreateTransferParams } from '../../domain/use-cases/CreateTransfer';
+import { HttpClient } from './../interfaces/HttpClient';
+
+const makeHttpClientStub = (): jest.Mocked<HttpClient> => ({ post: jest.fn() });
+
+const makeFakeTransferDataParams = (): CreateTransferParams => ({
+  externalId: 'any_external_id',
+  amount: 999,
+  expectedOn: new Date('03/01/2022'),
+});
+
+const makeSut = () => {
+  const httpClientStub = makeHttpClientStub();
+  const sut = new CreateTransferData(httpClientStub);
+
+  return { sut, httpClientStub };
+};
+
+describe(CreateTransferData.name, () => {
+  describe(CreateTransferData.prototype.create.name, () => {
+    it('Should throw when httpClient throws', async () => {
+      const { sut, httpClientStub } = makeSut();
+      const transferDataParams = makeFakeTransferDataParams();
+      httpClientStub.post.mockRejectedValueOnce(
+        new Error('any_httpClient_error')
+      );
+
+      const promise = sut.create(transferDataParams);
+
+      await expect(promise).rejects.toThrow(new Error('any_httpClient_error'));
+    });
+  });
+});
