690e7d5dc1d1f68b6a8d0a51b7b8eba3a172ac2d
lucsimao <lucasimao64@gmail.com>
2022-01-03 19:58:18 -0300

    test: create DateHelper
     - ensure return false when isAfter returns false
     - ensure return true when isAfter returns true
     - ensure throw when isAfter throws

---
+++ src/data/helpers/DateHelper.spec.ts
@@ -0,0 +1,47 @@
+import * as DateFnsModule from 'date-fns';
+
+import { DateHelper } from './DateHelper';
+
+jest.mock('date-fns');
+
+const makeSut = () => {
+  const sut = DateHelper;
+
+  return { sut };
+};
+
+describe(DateHelper.name, () => {
+  describe(DateHelper.isDateOverdue.name, () => {
+    it('Should return false when isAfter returns false', () => {
+      const { sut } = makeSut();
+      const date = new Date('01/01/2022');
+      jest.spyOn(DateFnsModule, 'isAfter').mockReturnValueOnce(false);
+
+      const result = sut.isDateOverdue(date);
+
+      expect(result).toBe(false);
+    });
+
+    it('Should return true when isAfter returns true', () => {
+      const { sut } = makeSut();
+      const date = new Date('05/01/2022');
+      jest.spyOn(DateFnsModule, 'isAfter').mockReturnValueOnce(true);
+
+      const result = sut.isDateOverdue(date);
+
+      expect(result).toBe(true);
+    });
+
+    it('Should throw when isAfter throws', () => {
+      const { sut } = makeSut();
+      const date = new Date('05/01/2022');
+      jest.spyOn(DateFnsModule, 'isAfter').mockImplementation(() => {
+        throw new Error('any_isAfter_error');
+      });
+
+      const result = sut.isDateOverdue;
+
+      expect(() => result(date)).toThrow(new Error('any_isAfter_error'));
+    });
+  });
+});
