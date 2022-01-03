6ab9c4a54651b933101faea4d292ed8a076576a4
Lucas Simão <lucas.simao@mutant.com.br>
2022-01-03 17:33:20 -0300

    refactor: ensure return 500 when CreateTransfer throws

--- src/presentation/controllers/CreateTransferController.spec.ts
+++ src/presentation/controllers/CreateTransferController.spec.ts
@@ -1,10 +1,20 @@
 import { CreateTransfer } from '../../domain/use-cases/CreateTransfer';
 import { CreateTransferController } from './CreateTransferController';
 import { HttpRequest } from '../interfaces/HttpRequest';
+import { TransferModel } from '../../domain/models/TransferModel';
+import { TransferStatusEnum } from '../../domain/enums/TransferStatusEnum';
 import httpStatusCodes from 'http-status-codes';
 
+const makeFakeTransferModel = (): TransferModel => ({
+  internalId: 'any_internal_id',
+  externalId: 'any_external_id',
+  amount: 999,
+  expectedOn: new Date('03/01/2022'),
+  status: TransferStatusEnum.CREATED,
+});
+
 const makeCreateTransferStub = (): jest.Mocked<CreateTransfer> => ({
-  create: jest.fn(),
+  create: jest.fn().mockResolvedValue(makeFakeTransferModel()),
 });
 
 const makeFakeHttpRequest = () => {
@@ -30,14 +40,17 @@ describe(CreateTransferController.name, () => {
     });
 
     it(`Should return ${httpStatusCodes.INTERNAL_SERVER_ERROR} when CreateTransfer throws`, async () => {
-      const { sut } = makeSut();
+      const { sut, createTransferStub } = makeSut();
       const fakeHttpRequest = makeFakeHttpRequest();
+      createTransferStub.create.mockRejectedValue(
+        new Error('any_create_error')
+      );
 
       const result = await sut.handle(fakeHttpRequest);
 
       expect(result).toEqual({
         statusCode: httpStatusCodes.INTERNAL_SERVER_ERROR,
-        body: { message: 'Internal server error' },
+        body: { message: new Error('any_create_error') },
       });
     });
   });
--- src/presentation/controllers/CreateTransferController.ts
+++ src/presentation/controllers/CreateTransferController.ts
@@ -1,18 +1,29 @@
-import { CreateTransfer } from '../../domain/use-cases/CreateTransfer';
+import {
+  CreateTransfer,
+  CreateTransferParams,
+} from '../../domain/use-cases/CreateTransfer';
+
 import { HttpRequest } from '../interfaces/HttpRequest';
 import { HttpResponse } from '../interfaces/HttpResponse';
-import { TransferModel } from '../../domain/models/TransferModel';
+import httpStatusCodes from 'http-status-codes';
 
 export class CreateTransferController {
   constructor(private readonly createTransfer: CreateTransfer) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const transfer = httpRequest.body as TransferModel;
-    await this.createTransfer.create(transfer);
+    try {
+      const transfer = httpRequest.body as CreateTransferParams;
+      await this.createTransfer.create(transfer);
 
-    return {
-      statusCode: 500,
-      body: { message: 'Internal server error' },
-    };
+      return {
+        statusCode: 500,
+        body: { message: 'Internal server error' },
+      };
+    } catch (error) {
+      return {
+        statusCode: httpStatusCodes.INTERNAL_SERVER_ERROR,
+        body: { message: error },
+      };
+    }
   }
 }
