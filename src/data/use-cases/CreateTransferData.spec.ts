e9f5968bbc68e36d2a7e6657939cc27c96e80cb7
lucsimao <lucasimao64@gmail.com>
2022-01-03 19:26:42 -0300

    test: ensure return httpClient response when success

--- src/data/use-cases/CreateTransferData.spec.ts
+++ src/data/use-cases/CreateTransferData.spec.ts
@@ -1,6 +1,8 @@
 import { CreateTransferData } from './CreateTransferData';
 import { CreateTransferParams } from '../../domain/use-cases/CreateTransfer';
 import { HttpClient } from './../interfaces/HttpClient';
+import { TransferModel } from './../../domain/models/TransferModel';
+import { TransferStatusEnum } from './../../domain/enums/TransferStatusEnum';
 
 const makeHttpClientStub = (): jest.Mocked<HttpClient> => ({ post: jest.fn() });
 
@@ -8,6 +10,14 @@ const makeFakeTransferDataParams = (): CreateTransferParams => ({
   externalId: 'any_external_id',
   amount: 999,
   expectedOn: new Date('03/01/2022'),
+});
+
+const makeFakeTransferModel = (): TransferModel => ({
+  internalId: 'any_internal_id',
+  externalId: 'any_external_id',
+  amount: 999,
+  expectedOn: new Date('03/01/2022'),
+  status: TransferStatusEnum.CREATED,
 });
 
 const makeSut = () => {
@@ -30,6 +40,16 @@ describe(CreateTransferData.name, () => {
         expectedOn: new Date('03/01/2022'),
         externalId: 'any_external_id',
       });
+    });
+
+    it('Should return httpClient response when success', async () => {
+      const { sut, httpClientStub } = makeSut();
+      const transferDataParams = makeFakeTransferDataParams();
+      httpClientStub.post.mockResolvedValueOnce(makeFakeTransferModel());
+
+      const result = await sut.create(transferDataParams);
+
+      expect(result).toEqual(makeFakeTransferModel());
     });
 
     it('Should throw when httpClient throws', async () => {
