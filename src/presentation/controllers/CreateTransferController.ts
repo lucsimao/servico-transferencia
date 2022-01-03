99e88984266983dae63184175933df9d55763cbe
Lucas Simão <lucas.simao@mutant.com.br>
2022-01-03 17:17:54 -0300

    feat: ensure call create when method is invoked

--- src/presentation/controllers/CreateTransferController.ts
+++ src/presentation/controllers/CreateTransferController.ts
@@ -1,8 +1,15 @@
+import { CreateTransfer } from '../../domain/use-cases/CreateTransfer';
 import { HttpRequest } from '../interfaces/HttpRequest';
 import { HttpResponse } from '../interfaces/HttpResponse';
+import { TransferModel } from '../../domain/models/TransferModel';
 
 export class CreateTransferController {
-  handle(_httpRequest: HttpRequest): HttpResponse {
+  constructor(private readonly createTransfer: CreateTransfer) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const transfer = httpRequest.body as TransferModel;
+    await this.createTransfer.create(transfer);
+
     return {
       statusCode: 500,
       body: { message: 'Internal server error' },
