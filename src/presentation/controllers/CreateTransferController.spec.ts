1110c31f515c5dc96732aebd5158bedd8a3bee9d
Lucas Simão <lucas.simao@mutant.com.br>
2022-01-03 17:18:22 -0300

    test: ensure call create when method is invoked

--- src/presentation/controllers/CreateTransferController.spec.ts
+++ src/presentation/controllers/CreateTransferController.spec.ts
@@ -1,19 +1,34 @@
+import { CreateTransfer } from '../../domain/use-cases/CreateTransfer';
 import { CreateTransferController } from './CreateTransferController';
 import { HttpRequest } from '../interfaces/HttpRequest';
 import httpStatusCodes from 'http-status-codes';
+
+const makeCreateTransferStub = (): jest.Mocked<CreateTransfer> => ({
+  create: jest.fn(),
+});
 
 const makeFakeHttpRequest = () => {
   return {} as HttpRequest;
 };
 
 const makeSut = () => {
-  const sut = new CreateTransferController();
+  const createTransferStub = makeCreateTransferStub();
+  const sut = new CreateTransferController(createTransferStub);
 
-  return { sut };
+  return { sut, createTransferStub };
 };
 
 describe(CreateTransferController.name, () => {
   describe(CreateTransferController.prototype.handle.name, () => {
+    it(`Should call create with correct params when method is invoked`, async () => {
+      const { sut, createTransferStub } = makeSut();
+      const fakeHttpRequest = makeFakeHttpRequest();
+
+      await sut.handle(fakeHttpRequest);
+
+      expect(createTransferStub.create).toBeCalledWith(fakeHttpRequest.body);
+    });
+
     it(`Should return ${httpStatusCodes.INTERNAL_SERVER_ERROR} when CreateTransfer throws`, async () => {
       const { sut } = makeSut();
       const fakeHttpRequest = makeFakeHttpRequest();
