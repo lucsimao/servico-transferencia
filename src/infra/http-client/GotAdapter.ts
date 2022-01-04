0c0de851a8f0b8212278d4e456d20a6e1b26772b
lucsimao <lucasimao64@gmail.com>
2022-01-03 21:53:14 -0300

    feat: create GotAdapter
    - get
      ensure return an object when method is called
      ensure call got.get when method is called with options
      ensure call got.get when method is called without options
    
    - post
      ensure return an object when method is called
      ensure call got.post when method is called with options
      ensure call got.post when method is called without options

---
+++ src/infra/http-client/GotAdapter.ts
@@ -0,0 +1,32 @@
+import got, { OptionsOfJSONResponseBody } from 'got';
+
+import { HttpClient } from './../../data/interfaces/HttpClient';
+import { HttpOptions } from './../../data/interfaces/HttpOptions';
+import { HttpResponse } from '../../data/interfaces/HttpResponse';
+
+export class GotAdapter implements HttpClient {
+  async get<T>(url: string, options?: HttpOptions): Promise<HttpResponse<T>> {
+    const gotOptions: OptionsOfJSONResponseBody = {
+      responseType: 'json',
+      ...(options as OptionsOfJSONResponseBody),
+    };
+    const result = await got.get<T>(url, gotOptions);
+
+    return { statusCode: result.statusCode, body: result.body as T };
+  }
+
+  public async post<T, K>(
+    url: string,
+    options?: HttpOptions
+  ): Promise<HttpResponse<K>> {
+    const gotOptions: OptionsOfJSONResponseBody = {
+      responseType: 'json',
+      json: options?.body as Record<string, T>,
+      headers: options?.headers,
+    };
+
+    const result = await got.post<K>(url, gotOptions);
+
+    return { statusCode: result.statusCode, body: result.body as K };
+  }
+}
