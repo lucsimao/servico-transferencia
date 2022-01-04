1526cc722f5f0ad579196502db8859bbb9eb8c59
lucsimao <lucasimao64@gmail.com>
2022-01-04 00:11:06 -0300

    feat: create TransferRoutes

---
+++ src/main/routes/TransferRoutes.ts
@@ -0,0 +1,9 @@
+import { Request, Response, Router } from 'express';
+
+export class TransferRoutes {
+  public static setRoutes(router: Router): void {
+    router.post('/transfer', (req: Request, res: Response) => {
+      res.json();
+    });
+  }
+}
